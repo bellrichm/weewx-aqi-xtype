5bedc566d9db3496f9c5020f9a6573d6a9777d1f
Rich Bell <bellrichm@gmail.com>
2023-07-06 08:18:42 -0400

    get_series and get_aggregate

--- bin/user/aqi.py
+++ bin/user/aqi.py
@@ -10,7 +10,9 @@ WeeWX XTypes extensions that add new types of AQI.
 import math
 import sys
 
+import weedb
 from weewx.engine import StdService
+from weewx.units import ValueTuple
 import weewx.xtypes
 
 class AQIService(StdService):
@@ -139,21 +141,23 @@ class EPAAQI(object):
         if aqi_type not in self.readings:
             raise weewx.CannotCalculate(obs_type)
 
-        breakpoint_count = len(self.readings)
+        readings = self.readings[aqi_type]
+
+        breakpoint_count = len(readings['breakpoints'])
         index = 0
         while index < breakpoint_count:
-            if reading < self.readings[aqi_type]['breakpoints'][index]['max']:
+            if reading < readings['breakpoints'][index]['max']:
                 break
             index += 1
 
         if index >= breakpoint_count:
-            index = breakpoint_count
+            index =  len(readings['breakpoints']) - 1
 
-        reading_bp = self.readings[aqi_type]['breakpoints'][index]
+        reading_bp = readings['breakpoints'][index]
         aqi_bp = self.aqi_bp[index]
 
-        aqi = round((aqi_bp['max'] - aqi_bp['min'])/(reading_bp['max'] - reading_bp['min']) * \
-              (reading - reading_bp['min']) + aqi_bp['min'])
+        aqi = round(((aqi_bp['max'] - aqi_bp['min'])/(reading_bp['max'] - reading_bp['min']) * \
+              (reading - reading_bp['min'])) + aqi_bp['min'])
 
         return aqi
 
@@ -169,6 +173,27 @@ class AQI(weewx.xtypes.XType):
         for field in self.aqi_fields:
             self.aqi_fields[field]["calculator"]  = \
                   getattr(sys.modules[__name__], self.aqi_fields[field]['algorithm'])()
+
+        self.sql_stmts = {
+        'avg': "SELECT AVG({input}) FROM {table_name} "
+               "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL",
+        'count': "SELECT COUNT(dateTime) FROM {table_name} "
+                 "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL",
+        'first': "SELECT {input} FROM {table_name} "
+                 "WHERE dateTime = (SELECT MIN(dateTime) FROM {table_name} "
+                 "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL",
+        'last': "SELECT {input} FROM {table_name} "
+                "WHERE dateTime = (SELECT MAX(dateTime) FROM {table_name} "
+                "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL",
+        'min': "SELECT {input} FROM {table_name} "
+               "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL "
+               "ORDER BY {input} ASC LIMIT 1;",
+        'max': "SELECT {input} FROM {table_name} "
+               "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL "
+               "ORDER BY {input} DESC LIMIT 1;",
+        'sum': "SELECT SUM({input}) FROM {table_name} "
+               "WHERE dateTime > {start} AND dateTime <= {stop} AND {input} IS NOT NULL)",
+    }
 
     def get_scalar(self, obs_type, record, db_manager=None, **option_dict):
         if obs_type not in self.aqi_fields:
@@ -189,4 +214,93 @@ class AQI(weewx.xtypes.XType):
                                                                 obs_type)
 
         unit_type, group = weewx.units.getStandardUnitType(record['usUnits'], obs_type)
+        return weewx.units.ValueTuple(aqi, unit_type, group)
+
+    def get_series(self, obs_type, timespan, db_manager, aggregate_type=None,
+                   aggregate_interval=None,
+                   **option_dict):
+
+        if obs_type not in self.aqi_fields:
+            raise weewx.UnknownType(obs_type)
+
+        aqi_type = self.aqi_fields[obs_type]['type']
+
+        dependent_field = self.aqi_fields[obs_type]["input"]
+
+        start_vec = list()
+        stop_vec = list()
+        data_vec = list()
+
+        if aggregate_type:
+            return weewx.xtypes.ArchiveTable.get_series(obs_type, timespan, db_manager,
+                                           aggregate_type, aggregate_interval, **option_dict)
+        else:
+            sql_str = 'SELECT dateTime, usUnits, `interval`, %s FROM %s ' \
+                      'WHERE dateTime >= ? AND dateTime <= ? AND %s IS NOT NULL' \
+                      % (dependent_field, db_manager.table_name, dependent_field)
+            std_unit_system = None
+
+            for record in db_manager.genSql(sql_str, timespan):
+                timestamp, unit_system, interval, input_value = record
+                if std_unit_system:
+                    if std_unit_system != unit_system:
+                        raise weewx.UnsupportedFeature(
+                            "Unit type cannot change within a time interval.")
+                else:
+                    std_unit_system = unit_system
+
+                    aqi = self.aqi_fields[obs_type]["calculator"].calculate(input_value,
+                                                            aqi_type,
+                                                            obs_type)
+
+                start_vec.append(timestamp - interval * 60)
+                stop_vec.append(timestamp)
+                data_vec.append(aqi)
+
+            unit, unit_group = weewx.units.getStandardUnitType(std_unit_system, obs_type,
+                                                               aggregate_type)
+
+        return (ValueTuple(start_vec, 'unix_epoch', 'group_time'),
+                    ValueTuple(stop_vec, 'unix_epoch', 'group_time'),
+                    ValueTuple(data_vec, unit, unit_group))
+
+    def get_aggregate(self, obs_type, timespan, aggregate_type, db_manager, **option_dict):
+        if obs_type not in self.aqi_fields:
+            raise weewx.UnknownType(obs_type)
+
+        if aggregate_type not in self.sql_stmts:
+            raise weewx.UnknownAggregation(aggregate_type)
+
+        dependent_field = self.aqi_fields[obs_type]["input"]
+        aqi_type = self.aqi_fields[obs_type]['type']
+
+        interpolation_dict = {
+            'start': timespan.start,
+            'stop': timespan.stop,
+            'table_name': db_manager.table_name,
+            'input': dependent_field
+        }
+
+        sql_stmt = self.sql_stmts[aggregate_type].format(**interpolation_dict)
+
+        try:
+            row = db_manager.getSql(sql_stmt)
+        except weedb.NoColumnError:
+            raise weewx.UnknownType(obs_type)
+
+        if not row or None in row:
+            input_value = None
+        else:
+            input_value = row[0]
+
+        if input_value is not None:
+            aqi = self.aqi_fields[obs_type]["calculator"].calculate(input_value,
+                                                    aqi_type,
+                                                    obs_type)
+        else:
+            aqi = None
+
+        unit_type, group = weewx.units.getStandardUnitType(db_manager.std_unit_system, obs_type,
+                                               aggregate_type)
+
         return weewx.units.ValueTuple(aqi, unit_type, group)
