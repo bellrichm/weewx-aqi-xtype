deb648f838eae44f8abd8f3bd7ee43128b56bf77
Rich Bell <bellrichm@gmail.com>
2023-07-07 11:06:13 -0400

    first test

---
+++ bin/user/tests/test_epaaqi.py
@@ -0,0 +1,18 @@
+
+# pylint: disable=missing-docstring
+
+import unittest
+import mock
+
+import user.aqitype
+
+class EPAAQITests(unittest.TestCase):
+
+    def test_first_breakpoint(self):
+        mock_logger = mock.Mock(spec=user.aqitype.Logger)
+        calculator = user.aqitype.EPAAQI(mock_logger)
+
+        self.assertEqual(calculator.calculate(5.0, 'pm2_5', 'foo'), 21)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
