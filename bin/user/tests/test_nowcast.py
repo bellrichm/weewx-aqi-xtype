50e0aa0a75bc4ad7ebe608bff4522e53016e74e8
Rich Bell <bellrichm@gmail.com>
2023-07-19 10:39:47 -0400

    cleanup test

--- bin/user/tests/test_nowcast.py
+++ bin/user/tests/test_nowcast.py
@@ -14,6 +14,14 @@ import weeutil
 import user.aqitype
 
 class EPAAQITests(unittest.TestCase):
+    def _populate_time_stamps(self, current_hour, count):
+        time_stamps = []
+        index = 0
+        while index < count:
+            time_stamps.append(current_hour - (count - index - 1) * 3600)
+            index += 1
+
+        return time_stamps
 
     def test_calculate_concentration(self):
         mock_logger = mock.Mock(spec=user.aqitype.Logger)
@@ -25,30 +33,9 @@ class EPAAQITests(unittest.TestCase):
                     current_hour =  int(now / 3600) * 3600
                     mock_start_of_imterval.return_value = current_hour
 
+                    data = [[123.3, 80.2, 49.3, 101.8, 93.7, 143.2, 215.4, 130.6, 129.2, 59.8, 27.4, 46.3]]
                     start_vec = None
-                    stop_vec = []
-                    stop_vec.append([])
-                    data = []
-                    data.append([])
-                    data[0].append(123.3)
-                    data[0].append(80.2)
-                    data[0].append(49.3)
-                    data[0].append(101.8)
-                    data[0].append(93.7)
-                    data[0].append(143.2)
-                    data[0].append(215.4)
-                    data[0].append(130.6)
-                    data[0].append(129.2)
-                    data[0].append(59.8)
-                    data[0].append(27.4)
-                    data[0].append(46.3)
-
-                    index = 0
-                    data_count = len(data[0])
-                    while index < data_count:
-                        stop_vec[0].append(current_hour - (data_count - index - 1) * 3600)
-                        index += 1
-
+                    stop_vec = [self._populate_time_stamps(current_hour, len(data[0]))]
                     mock_xtype.return_value.get_series.return_value = start_vec, stop_vec, data
 
                     calculator = user.aqitype.NOWCAST(mock_logger, None, None)
