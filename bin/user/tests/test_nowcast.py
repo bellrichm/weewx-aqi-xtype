4650b30bf6d41e91730aa2f526f842a2beccc866
Rich Bell <bellrichm@gmail.com>
2023-07-20 10:17:37 -0400

    linting

--- bin/user/tests/test_nowcast.py
+++ bin/user/tests/test_nowcast.py
@@ -27,14 +27,14 @@ class NowCastTests(unittest.TestCase):
             index += 1
 
         return time_stamps
-    
+
     def test_invalid_time_stamp(self):
         mock_logger = mock.Mock(spec=user.aqitype.Logger)
 
         with self.assertRaises(weewx.CannotCalculate):
             calculator = user.aqitype.NOWCAST(mock_logger, 0, None, None)
 
-            calculator.calculate(None, None, None, random_string())    
+            calculator.calculate(None, None, None, random_string())
 
     def test_invalid_type(self):
         mock_logger = mock.Mock(spec=user.aqitype.Logger)
