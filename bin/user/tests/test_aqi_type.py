e371dcb967d3dab88707519d2decf7b3c6bf2b7b
Rich Bell <bellrichm@gmail.com>
2025-02-19 07:51:30 -0500

    test calculate pm2_5 aqi

--- bin/user/tests/test_aqi_type.py
+++ bin/user/tests/test_aqi_type.py
@@ -4,53 +4,72 @@
 #    See the file LICENSE.txt for your full rights.
 #
 
-# This file is to see if there is any reasonable way to write tests for the AQIType class
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-docstring
+# pylint: disable=invalid-name
 
 import unittest
 import mock
 
 import configobj
+import random
+import string
 import time
 
 import user.aqitype
 
-def setup_config():
+def random_string(length=32):
+    return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(length)]) # pylint: disable=unused-variable
+
+def setup_config(calculated_field, input_field, algorithm, aqi_type):
     config_dict = {
-        'pm2_5_aqi': {
-            'input': 'pm2_5',
-            'algorithm': 'EPAAQI',
-            'type': 'pm2_5',
+        calculated_field: {
+            'input': input_field,
+            'algorithm': algorithm,
+            'type': aqi_type,
         }
     }
-
     return config_dict
 
-class Test01(unittest.TestCase):
-    @unittest.skip("Under development")
-    def test01(self):
-        print("test01:test01")
+class TestEPAAQICalculate(unittest.TestCase):
+    def test_get_scalar_valid_inputs(self):
         mock_logger = mock.Mock(spec=user.aqitype.Logger)
 
-        config_dict = setup_config()
+        algorithm = 'EPAAQI'
+        aqi_type = 'pm2_5'
+
+        calculated_field = random_string()
+        input_field = random_string()
+
+        config_dict = setup_config(calculated_field, input_field, algorithm, aqi_type)
         config = configobj.ConfigObj(config_dict)
 
-        SUT = user.aqitype.AQIType(mock_logger, config)
+        aqi = random.randint(11, 100)
+        with mock.patch.object(user.aqitype.EPAAQI, 'calculate', return_value=aqi):
+            SUT = user.aqitype.AQIType(mock_logger, config)
 
-        record = {
-            'usUnits': 1,
-            'interval': 5,
-            'dateTime': time.time(),
-            'pm2_5': 10,
-        }
+            unit = random_string()
+            unit_group = random_string()
 
-        aqi = SUT.get_scalar('pm2_5_aqi', record)
-        print(aqi)
+            record = {
+                'usUnits': 1,
+                'interval': 5,
+                'dateTime': time.time(),
+                input_field: random.randint(0, 10),
+            }
 
-        print("test01:test01")
+            with mock.patch('weewx.units.getStandardUnitType') as mock_get_standard_unit_type:
+                mock_get_standard_unit_type.return_value = [unit, unit_group]
+
+                value_tuple = SUT.get_scalar(calculated_field, record)
+
+                self.assertEqual(value_tuple[0], aqi)
+                self.assertEqual(value_tuple[1], unit)
+                self.assertEqual(value_tuple[2], unit_group)
 
 if __name__ == '__main__':
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(Test01('test01'))
-    unittest.TextTestRunner().run(test_suite)
+    #test_suite = unittest.TestSuite()
+    #test_suite.addTest(TestEPAAQICalculate('test_get_scalar_valid_inputs'))
+    #unittest.TextTestRunner().run(test_suite)
 
-    #unittest.main(exit=False)
+    unittest.main(exit=False)
