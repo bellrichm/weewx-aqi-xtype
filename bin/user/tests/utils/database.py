048cbec34aca063ef9c58fe2a48ac7439aedfc69
Rich Bell <bellrichm@gmail.com>
2025-03-02 08:06:13 -0500

    put 02/20 data into test database

--- bin/user/tests/utils/database.py
+++ bin/user/tests/utils/database.py
@@ -31,13 +31,15 @@ timespan = weeutil.weeutil.TimeSpan(
 
 def _generate_records(pm2_5_column):
     ''' Generate records to be inserted into a WeeWX database. '''
+    pm2_5_values =  data.db_20250220_pm2_5_values + data.db_20250221_pm2_5_values
+    timestamps = data.db__20250220_timestamps + data.db_20250221_timestamps
     i = 0
-    for date_time in data.db_20250221_timestamps:
+    for date_time in timestamps:
         yield {
             'dateTime': date_time,
             'usUnits': US_UNITS,
             'interval': ARCHIVE_INTERVAL_MINUTES,
-            pm2_5_column: data.db_20250221_pm2_5_values[i],
+            pm2_5_column: pm2_5_values[i],
         }
         i += 1
 
